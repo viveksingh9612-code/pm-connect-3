accc364e2624d5e0cfc5ea6e533d5b880edfb346
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2025-09-24 18:13:32 +0530

    image updated, documents added

--- src/app/app/features/dashboard/dashboard.component.ts
+++ src/app/app/features/dashboard/dashboard.component.ts
@@ -1,20 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
+
+
+interface GalleryImage {
+  src: string;
+  alt: string;
+}
+
+interface Image {
+  src: string;
+  alt: string;
+}
+
+interface Document {
+  name: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
-  videos = [
-    { title: 'Welcome to Jakson Infra', url: 'https://www.youtube.com/embed/dQw4w9WgXcQ' },
-    { title: 'Event Highlights', url: 'https://www.youtube.com/embed/3GwjfUFyY6M' }
-    // ...add more as needed
+
+export class DashboardComponent implements OnInit{
+  videos: { title: string, url: SafeResourceUrl }[] = [];
+
+  selectedImage: Image | null = null;
+
+  selectedDoc: { name: string, url: SafeResourceUrl } | null = null;
+
+  images: GalleryImage[] = [
+    { src: '/assets/eventpics/1/event1.jpg', alt: 'Event Image 1' },
+    { src: '/assets/eventpics/1/event2.jpg', alt: 'Event Image 2' },
+    { src: '/assets/eventpics/1/event3.jpg', alt: 'Event Image 3' },
+    { src: '/assets/eventpics/1/event4.jpg', alt: 'Event Image 4' },
+    { src: '/assets/eventpics/2/event1.jpg', alt: 'Event Image 5' },
+    { src: '/assets/eventpics/2/event2.jpg', alt: 'Event Image 6' }
+  ]
+
+  rawVideos = [
+    { title: 'Jakson Group', url: 'https://www.youtube.com/embed/eG3LkObFCO4' },
+    { title: 'JAKSON - The Journey of 75+ years', url: 'https://www.youtube.com/embed/Vc_Tb45aUF4' }
   ];
 
   documents = [
-    { name: 'Event Schedule.pdf', url: '/assets/documents/event-schedule.pdf' },
-    { name: 'Venue Map.pdf', url: '/assets/documents/venue-map.pdf' }
+    { name: 'Agenda', url: '/assets/documents/agenda.pdf' },
+    { name: 'Safety Booklet', url: '/assets/documents/jaksonsafetybooklet.pdf' },
+    { name: 'JJM Qality Handbook', url: '/assets/documents/qualityhandbookjmm.pdf' },
+    { name: 'RDSS Quality Handbook', url: '/assets/documents/qualityhandbookrdss.pdf' },
+    { name: 'Safety Flayer', url: '/assets/documents/safetyflayer.pdf' }
     // ...add more as needed
   ];
+
+  constructor(
+    private sanitizer: DomSanitizer
+  ) {
+    this.videos = this.rawVideos.map(video => ({
+      title: video.title,
+      url: this.sanitizer.bypassSecurityTrustResourceUrl(video.url)
+    }));
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  openModal(img: Image) {
+    this.selectedImage = img;
+  }
+
+  closeModal() {
+    this.selectedImage = null;
+  }
+
+  openDoc(doc: Document) {
+    this.selectedDoc = {
+      name: doc.name,
+      url: this.sanitizer.bypassSecurityTrustResourceUrl(doc.url)
+    };
+  }
+
+  closeDoc() {
+    this.selectedDoc = null;
+  }
 }
